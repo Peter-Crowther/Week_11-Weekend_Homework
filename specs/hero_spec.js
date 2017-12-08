a3107a31c0052013692aff3e612cf02e1e3c9762
Peter Crowther <petercrowther90@hotmail.com>
2017-12-08 11:21:04 +0000

    tested name, health, favfood and array

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -5,11 +5,18 @@ describe("Hero", function(){
   var hero;
 
   beforeEach(function() {
-    hero = new Hero("SuperTed", 100, "Lasagne")
+    hero = new Hero("SuperTed", 100, "Lasagne", [])
+  });
+
+  it("should have a name, health, favourite food and empty tasks", function(){
+    assert.strictEqual(hero.name, "SuperTed");
+    assert.strictEqual(hero.health, 100);
+    assert.strictEqual(hero.favourite_food, "Lasagne")
+    assert.deepStrictEqual(hero.hero_tasks.length, 0)
   });
 
   it("should be able to talk", function() {
-    assert.strictEqual(hero.talk(), "My name is SuperTed")
+    assert.strictEqual(hero.talk(), "My name is SuperTed");
   });
 
 });
